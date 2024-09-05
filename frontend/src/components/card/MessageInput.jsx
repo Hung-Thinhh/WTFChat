7789e9d1cb5e9024bda2c389055e39b72a382bcc
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-09-05 18:09:18 +0700

    longdaide

--- frontend/src/components/card/MessageInput.jsx
+++ frontend/src/components/card/MessageInput.jsx
@@ -1,8 +1,9 @@
+import "../../css/MessageInput.scss";
 const MessageInput = () => {
     return (
-        <>
+        <div className="input_container">
             <input type="text" />
-        </>
+        </div>
     );
 }
 
--- frontend/src/components/pages/chatPage.jsx
+++ frontend/src/components/pages/chatPage.jsx
@@ -1,10 +1,14 @@
 import "../../css/ChatPage.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import MessageBubble from "../card/MessageBubble";
 import MessageInput from "../card/MessageInput";
 const ChatPage = () => {
     return (
         <div className="chatPage_container">
-            <div className="chatpage_header"></div>
+            <div className="chatpage_header">
+            <FontAwesomeIcon icon={faUser} className="chatPage_chat_avt"/>  4 anh em bị gà
+            </div>
             <div className="ChatWindow">
                 <MessageBubble />
             </div>
