8c37759cc841ae01eef2b27f0f8f857f5ac4610a
dragonccm <121989450+dragonccm@users.noreply.github.com>
2024-09-20 09:13:37 +0700

    .

--- frontend/src/components/card/MessageBubble.jsx
+++ frontend/src/components/card/MessageBubble.jsx
@@ -1,83 +1,27 @@
 import React from 'react';
+import '../../css/MessageBubble.scss';
 
 const MessageBubble = (data) => {
+    const userClass = data.data.user === "me" ? "me" : "other";
+
     return (
-        <div
-            style={
-                data.data.user === "me" ?
-                    {
-                        width: "100%",
-                        display: "flex",
-                        justifyContent: "flex-start",
-                        alignItems: "center",
-                        flexDirection: "row",
-                        margin: "2rem 1rem"
-                    }
-                    :
-                    {
-                        width: "100%",
-                        display: "flex",
-                        justifyContent: "flex-end",
-                        alignItems: "center",
-                        flexDirection: "row",
-                        margin: "2rem 1rem"
-                    }
-            }
-            className='messageBubble'
-        >
-            <div
-                style={
-                    data.data.user === "me" ?
-                        {
-                            maxWidth: '50%',
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: "space-between",
-                            gap: "1rem",
-                            alignItems: "flex-end",
-                        }
-                        :
-                        {
-                            maxWidth: '50%',
-                            display: 'flex',
-                            flexDirection: 'row-reverse',
-                            justifyContent: "space-between",
-                            gap: "1rem",
-                            alignItems: "flex-end",
-                            float: "right",
-                        }
-                }
-            >
-                <div style={{ margin: "1rem 0", display: 'flex', flexDirection: 'row', gap: '1rem' }}>
-                    <img alt="UserAvt" src={data.data.avt} style={{ borderRadius: '50%', width: '40px', height: '40px' }} />
+        <div className={`messageBubble ${userClass}`}>
+            <div className={`messageContent ${userClass}`}>
+                <div className="userAvatar">
+                    <img alt="UserAvt" src={data.data.avt} />
                 </div>
-                <div style={{
-                    display: 'flex',
-                    minWidth: '15rem',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    border: '1px solid #ccc',
-                    borderRadius: '8px',
-                    overflow: 'hidden',
-                    backgroundColor: data.data.user==="me" ? "#626262": "rgb(208 188 255)",
-                    borderRadius: "8px",
-                    border:"none",
-                    color:data.data.user==="me" ? "white": "black",
-                }}>
-                    <img
-                        style={{
-                            maxWidth: "50%",
-                        }}
-                        src="https://meliawedding.com.vn/wp-content/uploads/2022/03/avatar-gai-xinh-1.jpg"
-                        alt="Live from space album cover"
-                    />
-                    <div style={{ display: 'flex', flexDirection: 'column', width: "100%", justifyContent: "flex-end", paddingRight: "1rem" }}>
-                        <div style={{ flex: '1 0 auto', padding: '1rem' }}>
-                            <h6 style={{ margin: 0 ,fontSize:"1.2rem", fontWeight:"500"}}>
-                                {data.data.content}
-                            </h6>
+                <div className={`messageBox ${userClass}`}>
+                    {data.data.img !== '' && (
+                        <img
+                            src={data.data.img}
+                            alt="Live from space album cover"
+                        />
+                    )}
+                    <div className="messageText">
+                        <div>
+                            <p>{data.data.content}</p>
                         </div>
-                        <div style={{ display: 'flex', width: "100%", paddingLeft: '1rem', paddingBottom: '1rem' }}>
+                        <div className="messageTime">
                             {data.data.time} phút trước
                         </div>
                     </div>
--- frontend/src/components/pages/chatPage.jsx
+++ frontend/src/components/pages/chatPage.jsx
@@ -1,11 +1,8 @@
 import "../../css/ChatPage.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle, faPaperPlane, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCircle, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import MessageBubble from "../card/MessageBubble";
 import MessageInput from "../card/MessageInput";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
 const ChatPage = () => {
     return (
         <div className="chatPage_container">
@@ -23,14 +20,16 @@ const ChatPage = () => {
             <div className="ChatWindow">
                 <MessageBubble data={
                     {
+                        img:"",
                         avt: "https://meliawedding.com.vn/wp-content/uploads/2022/03/avatar-gai-xinh-1.jpg",
-                        content: "ĐỊT CON ĐĨ MẸ NHÀ MÀY LÚC SÚC VẬT, WIBU THÌ ĐÃ SAO HẢ MẤY CON CHÓ ĂN CỨC RẢNH LỒN KHÔNG CÓ CHUYỆN GÌ LÀM ĐI GATO VS WIBU HẢ? WIBU ĂN HẾT CÁI LỒN CON ĐĨ MẸ MÀY HAY GÌ CỨ HỞ TÍ WIBU LÀ SAO HẢ CÁI CON THÚ HOANG RÁC RƯỞI, ĐỊT HẾT CÁC ĐỜI TỔ TÔNG GIA PHẢ NHÀ CON ĐĨ MẸ MÀY, TAO WIBU THÌ SAO? TỤI MÀY KO ĐC LÀM WIBU NHƯ TỤI TAO RỒI TỤI M TỨK HẢ? TỤI MÀY ĐÉO CÓ GỐI ÔM CỦA REM ĐỂ ĐỤ NÊN TỨK HẢ? TỤI MÀY ĐÉO CÓ THIỂU NĂNG NHƯ TỤI TAO TỤI MÀY TỨK HAY GÌ? TAO LÀ 1 WIBU CHÂN CHÍNH NÊN ĐỪNG ĐỤNG VÔ TỤI TAO, NẾU CÒN ĐỤNG VÔ THÌ TAO SẼ HOÁ ZORO CẦM 3 THANH KIẾM CHÉM MÀY RA HÀNG TRĂM MẢNH RỒI CHO CÁ SẤU ĂN ĐÓ, ĐỤ MẸ TỤI T NGỒI K CŨNG CÓ ĂN NÈ ĐÂU NHƯ TỤI M LÀM NHƯ CHÓ TỚI CUỐI THÁNG MỚI CÓ LƯƠNG ĐÂU, TỤI TAO NGỒI K ĂN BÁT VÀNG NÈ CON ĐĨ MẸ TỤI MÀY, TAO TỨK QUÁ MÀ, DÒNG ĐĨ NỨNG LỒN, NỨNG CẶC GÌ ĐÂU K À 😡😡😡😡😡😡😡",
+                        content: "ĐỊT CON ĐĨ MẸ NHÀ MÀY LÚC SÚC VẬT",
                         time: "25:00",
                         user: "i"
                     }
                 } />
                 <MessageBubble data={
                     {
+                        img:"https://meliawedding.com.vn/wp-content/uploads/2022/03/avatar-gai-xinh-1.jpg",
                         avt: "https://meliawedding.com.vn/wp-content/uploads/2022/03/avatar-gai-xinh-1.jpg",
                         content: "ĐỊT CON ĐĨ MẸ NHÀ MÀY LÚC SÚC VẬT, WIBU THÌ ĐÃ SAO HẢ MẤY CON CHÓ ĂN CỨC RẢNH LỒN KHÔNG CÓ CHUYỆN GÌ LÀM ĐI GATO VS WIBU HẢ? WIBU ĂN HẾT CÁI LỒN CON ĐĨ MẸ MÀY HAY GÌ CỨ HỞ TÍ WIBU LÀ SAO HẢ CÁI CON THÚ HOANG RÁC RƯỞI, ĐỊT HẾT CÁC ĐỜI TỔ TÔNG GIA PHẢ NHÀ CON ĐĨ MẸ MÀY, TAO WIBU THÌ SAO? TỤI MÀY KO ĐC LÀM WIBU NHƯ TỤI TAO RỒI TỤI M TỨK HẢ? TỤI MÀY ĐÉO CÓ GỐI ÔM CỦA REM ĐỂ ĐỤ NÊN TỨK HẢ? TỤI MÀY ĐÉO CÓ THIỂU NĂNG NHƯ TỤI TAO TỤI MÀY TỨK HAY GÌ? TAO LÀ 1 WIBU CHÂN CHÍNH NÊN ĐỪNG ĐỤNG VÔ TỤI TAO, NẾU CÒN ĐỤNG VÔ THÌ TAO SẼ HOÁ ZORO CẦM 3 THANH KIẾM CHÉM MÀY RA HÀNG TRĂM MẢNH RỒI CHO CÁ SẤU ĂN ĐÓ, ĐỤ MẸ TỤI T NGỒI K CŨNG CÓ ĂN NÈ ĐÂU NHƯ TỤI M LÀM NHƯ CHÓ TỚI CUỐI THÁNG MỚI CÓ LƯƠNG ĐÂU, TỤI TAO NGỒI K ĂN BÁT VÀNG NÈ CON ĐĨ MẸ TỤI MÀY, TAO TỨK QUÁ MÀ, DÒNG ĐĨ NỨNG LỒN, NỨNG CẶC GÌ ĐÂU K À 😡😡😡😡😡😡😡",
                         time: "25:00",
@@ -39,8 +38,9 @@ const ChatPage = () => {
                 } />
                 <MessageBubble data={
                     {
+                        img:"https://meliawedding.com.vn/wp-content/uploads/2022/03/avatar-gai-xinh-1.jpg",
                         avt: "https://meliawedding.com.vn/wp-content/uploads/2022/03/avatar-gai-xinh-1.jpg",
-                        content: "ĐỊT CON ĐĨ MẸ NHÀ MÀY LÚC SÚC VẬT, WIBU THÌ ĐÃ SAO HẢ MẤY CON CHÓ ĂN CỨC RẢNH LỒN KHÔNG CÓ CHUYỆN GÌ LÀM ĐI GATO VS WIBU HẢ? WIBU ĂN HẾT CÁI LỒN CON ĐĨ MẸ MÀY HAY GÌ CỨ HỞ TÍ WIBU LÀ SAO HẢ CÁI CON THÚ HOANG RÁC RƯỞI, ĐỊT HẾT CÁC ĐỜI TỔ TÔNG GIA PHẢ NHÀ CON ĐĨ MẸ MÀY, TAO WIBU THÌ SAO? TỤI MÀY KO ĐC LÀM WIBU NHƯ TỤI TAO RỒI TỤI M TỨK HẢ? TỤI MÀY ĐÉO CÓ GỐI ÔM CỦA REM ĐỂ ĐỤ NÊN TỨK HẢ? TỤI MÀY ĐÉO CÓ THIỂU NĂNG NHƯ TỤI TAO TỤI MÀY TỨK HAY GÌ? TAO LÀ 1 WIBU CHÂN CHÍNH NÊN ĐỪNG ĐỤNG VÔ TỤI TAO, NẾU CÒN ĐỤNG VÔ THÌ TAO SẼ HOÁ ZORO CẦM 3 THANH KIẾM CHÉM MÀY RA HÀNG TRĂM MẢNH RỒI CHO CÁ SẤU ĂN ĐÓ, ĐỤ MẸ TỤI T NGỒI K CŨNG CÓ ĂN NÈ ĐÂU NHƯ TỤI M LÀM NHƯ CHÓ TỚI CUỐI THÁNG MỚI CÓ LƯƠNG ĐÂU, TỤI TAO NGỒI K ĂN BÁT VÀNG NÈ CON ĐĨ MẸ TỤI MÀY, TAO TỨK QUÁ MÀ, DÒNG ĐĨ NỨNG LỒN, NỨNG CẶC GÌ ĐÂU K À 😡😡😡😡😡😡😡",
+                        content: "",
                         time: "25:00",
                         user: "i"
                     }
