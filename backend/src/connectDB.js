6436f2288224f10a398d9afdb299a7bca69803a7
dawn1810 <binhminh19112003@gmail.com>
2024-10-03 18:28:41 +0700

    fix register

--- backend/src/connectDB.js
+++ backend/src/connectDB.js
@@ -1,25 +1,60 @@
-import mysql from "mysql2/promise";
-import util from "util";
-require("dotenv").config();
+import mysql from 'mysql2/promise';
+import util from 'util';
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.SQL_HOST,
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASS,
-  database: process.env.SQL_DBNAME,
-  port: process.env.SQL_PORT,
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DBNAME,
+    port: process.env.SQL_PORT,
 });
 
-export const queryAsync = util.promisify(pool.query).bind(pool);
+// export const connection = () => {
+//     return new Promise((resolve, reject) => {
+//         pool.getConnection((err, connection) => { 
+//             if (err) reject(err);
+//             console.log('MySQL pool connected: threadId ' + connection.threadId);
+//             const query = (sql, binding) => {
+//                 return new Promise((resolve, reject) => {
+//                     connection.query(sql, binding, (err, result) => {
+//                         if (err) reject(err);
+//                         resolve(result);
+//                     });
+//                 });
+//             };
+//             const release = () => {
+//                 return new Promise((resolve, reject) => {
+//                     if (err) reject(err);
+//                     console.log('MySQL pool released: threadId ' + connection.threadId);
+//                     resolve(connection.release());
+//                 });
+//             };
+//             resolve({ query, release });
+//         });
+//     });
+// };
 
-pool
-  .getConnection()
-  .then((connection) => {
-    console.log("Kết nối thành DB công!");
-    connection.release();
-  })
-  .catch((err) => {
-    console.error("Kết nối thất bại:", err);
-  });
+
+// code nay ko quan trong
+export const query = (sql, binding) => {
+    return new Promise((resolve, reject) => {
+        pool.query(sql, binding, (err, result, fields) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+};
+
+pool.getConnection()
+    .then((connection) => {
+        console.log('Kết nối thành DB công!');
+        connection.release();
+    })  
+    .catch((err) => {  
+        console.error('Kết nối thất bại:', err);
+    });
+
+// export const queryAsync = pool.query;
 
 export default pool;
--- backend/src/services/Authentication_service.js
+++ backend/src/services/Authentication_service.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 import bcrypt from 'bcryptjs';
-import User from '../models/user_model.js';
 // const { getGroupWithRole } = require("./jwt-services.js");
 const { createToken } = require('../middleware/jwt.js');
 // get the promise implementation, we will use bluebird
 const salt = bcrypt.genSaltSync(10);
 const hash = bcrypt.hashSync('B4c0//', salt);
 import { v4 as uuidv4 } from 'uuid';
-import { queryAsync } from '../connectDB.js';
+import { getAge } from '../lib/globalFunct.js';
+import { query } from '../connectDB.js';
 
 const hashPassword = (password) => {
     const pass_hash = bcrypt.hashSync(password, salt);
@@ -17,14 +17,14 @@ const hashPassword = (password) => {
 const checkEmail = async (email) => {
     // add check by sending email add later
     try {
-        await queryAsync('START TRANSACTION');
-        const user = await queryAsync(`SELECT email FROM xacthuc WHERE email = ?`, [email]);
+        await query('START TRANSACTION');
+        const user = await query(`SELECT email FROM xacthuc WHERE email = ?`, [email]);
 
-        await queryAsync('COMMIT');
+        await query('COMMIT');
 
         return user.length > 0;
     } catch (err) {
-        await queryAsync('ROLLBACK');
+        await query('ROLLBACK');
 
         console.log('CHECK_EMAIL | ERROR | ', err);
         return false;
@@ -42,7 +42,7 @@ const generateId = () => {
 const handleRegister = async (data) => {
     if (!data)
         return {
-            EM: 'No Data',
+            EM: 'REGISTER | ERROR | No Data',
             EC: '400',
         };
 
@@ -68,7 +68,7 @@ const handleRegister = async (data) => {
             EM: "REGISTER | ERROR | Birthdate can't empty",
             EC: '400',
         };
-    else if (getAge(input.birthdate) <= 13)
+    else if (getAge(birthdate) <= 13)
         return {
             EM: 'REGISTER | ERROR | User ages must higher than 13',
             EC: '400',
@@ -79,7 +79,10 @@ const handleRegister = async (data) => {
             EC: '400',
         };
 
+    console.log('aaaa');
+
     const isEmailExist = await checkEmail(data.email);
+    console.log(isEmailExist);
 
     if (isEmailExist) {
         return {
@@ -88,13 +91,13 @@ const handleRegister = async (data) => {
         };
     } else {
         try {
-            await queryAsync('START TRANSACTION');
+            await query('START TRANSACTION');
             const hashPass = hashPassword(data.password);
             const firstName = username.split(' ').slice(0, -1).join(' ');
             const lastName = username.split(' ').slice(-1).join(' ');
 
             // insert user information
-            await queryAsync(
+            await query(
                 `INSERT INTO  nguoidung (firstname, lastname, email, birthdate, gender) VALUES (
 					?,
 					?, 
@@ -106,7 +109,7 @@ const handleRegister = async (data) => {
             );
 
             // insert user authen
-            await queryAsync(
+            await query(
                 `INSERT INTO  xacthuc (email, password, EC) VALUES (
 					?,
 					?, 
@@ -120,7 +123,7 @@ const handleRegister = async (data) => {
                 EC: '200',
             };
         } catch (error) {
-            await queryAsync('ROLLBACK');
+            await query('ROLLBACK');
 
             console.log('SERVICE | REGISTER | ERROR | ', error);
             return {
@@ -131,120 +134,120 @@ const handleRegister = async (data) => {
     }
 };
 
-const handleLogin = async (data) => {
-    try {
-        const user = await User.findOne({
-            $or: [{ email: data.valueLogin }, { username: data.valueLogin }],
-        });
-        if (user) {
-            let isCorrectPassword = await checkPassword(data.password, user.password);
-            if (isCorrectPassword) {
-                // let groupWithRole = await getGroupWithRole(user);
-                let payload = {
-                    id: user.id,
-                    email: user.email,
-                    type_login: user.type_login,
-                    username: user.username,
-                };
-                let token = createToken(payload);
-                return {
-                    EM: 'ok!',
-                    EC: '0',
-                    DT: {
-                        access_token: token,
-                        avt: user.avt,
-                        email: user.email,
-                        username: user.username,
-                    },
-                };
-            }
-        } else {
-            return {
-                EM: 'Your email/phone or password is incorrect!',
-                EC: '2',
-                DT: '',
-            };
-        }
+// const handleLogin = async (data) => {
+//     try {
+//         const user = await User.findOne({
+//             $or: [{ email: data.valueLogin }, { username: data.valueLogin }],
+//         });
+//         if (user) {
+//             let isCorrectPassword = await checkPassword(data.password, user.password);
+//             if (isCorrectPassword) {
+//                 // let groupWithRole = await getGroupWithRole(user);
+//                 let payload = {
+//                     id: user.id,
+//                     email: user.email,
+//                     type_login: user.type_login,
+//                     username: user.username,
+//                 };
+//                 let token = createToken(payload);
+//                 return {
+//                     EM: 'ok!',
+//                     EC: '0',
+//                     DT: {
+//                         access_token: token,
+//                         avt: user.avt,
+//                         email: user.email,
+//                         username: user.username,
+//                     },
+//                 };
+//             }
+//         } else {
+//             return {
+//                 EM: 'Your email/phone or password is incorrect!',
+//                 EC: '2',
+//                 DT: '',
+//             };
+//         }
 
-        return {
-            EM: 'Your email/phone or password is incorrect!',
-            EC: '1',
-            DT: '',
-        };
-    } catch (error) {
-        console.log('error: >>>>', error);
-        return {
-            EM: 'error creating user',
-            EC: '2',
-            DT: '',
-        };
-    }
-};
-const handleAuthGG = async (token) => {
-    try {
-        const user = await User.findOne({
-            token: token,
-        });
-        console.log(user);
-        if (user) {
-            user.token = generateId();
-            await user.save();
-            // let groupWithRole = await getGroupWithRole(user);
-            let payload = {
-                id: user.id,
-                email: user.email,
-                username: user.username,
-                type_login: user.type_login,
-            };
-            let token = createToken(payload);
-            return {
-                EM: 'ok!',
-                EC: '0',
-                DT: {
-                    access_token: token,
-                    avt: user.avt,
-                    email: user.email,
-                    username: user.username || '',
-                },
-            };
-        } else {
-            console.log('hahah');
-            return {
-                EM: 'Your email/phone or password is incorrect!',
-                EC: '2',
-                DT: '',
-            };
-        }
-    } catch (error) {
-        console.log('error: >>>>', error);
-        return {
-            EM: 'error creating user',
-            EC: '2',
-            DT: '',
-        };
-    }
-};
-const handleCheckAccount = async (id) => {
-    const user = await User.findOne({
-        id: id,
-    });
-    // console.log(user);
-    // console.log(id);
-    if (user) {
-        return {
-            EM: 'ok!',
-            EC: '0',
-            DT: user,
-        };
-    }
-};
-module.exports = {
+//         return {
+//             EM: 'Your email/phone or password is incorrect!',
+//             EC: '1',
+//             DT: '',
+//         };
+//     } catch (error) {
+//         console.log('error: >>>>', error);
+//         return {
+//             EM: 'error creating user',
+//             EC: '2',
+//             DT: '',
+//         };
+//     }
+// };
+// const handleAuthGG = async (token) => {
+//     try {
+//         const user = await User.findOne({
+//             token: token,
+//         });
+//         console.log(user);
+//         if (user) {
+//             user.token = generateId();
+//             await user.save();
+//             // let groupWithRole = await getGroupWithRole(user);
+//             let payload = {
+//                 id: user.id,
+//                 email: user.email,
+//                 username: user.username,
+//                 type_login: user.type_login,
+//             };
+//             let token = createToken(payload);
+//             return {
+//                 EM: 'ok!',
+//                 EC: '0',
+//                 DT: {
+//                     access_token: token,
+//                     avt: user.avt,
+//                     email: user.email,
+//                     username: user.username || '',
+//                 },
+//             };
+//         } else {
+//             console.log('hahah');
+//             return {
+//                 EM: 'Your email/phone or password is incorrect!',
+//                 EC: '2',
+//                 DT: '',
+//             };
+//         }
+//     } catch (error) {
+//         console.log('error: >>>>', error);
+//         return {
+//             EM: 'error creating user',
+//             EC: '2',
+//             DT: '',
+//         };
+//     }
+// };
+// const handleCheckAccount = async (id) => {
+//     const user = await User.findOne({
+//         id: id,
+//     });
+//     // console.log(user);
+//     // console.log(id);
+//     if (user) {
+//         return {
+//             EM: 'ok!',
+//             EC: '0',
+//             DT: user,
+//         };
+//     }
+// };
+export const services = {
     handleRegister,
-    handleLogin,
-    handleAuthGG,
+    // handleLogin,
+    // handleAuthGG,
     checkEmail,
     generateId,
     checkPassword,
     hashPassword,
-    handleCheckAccount,
+    // handleCheckAccount,
 };
