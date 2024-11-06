5813e17807fb16017ee58683fae953014e65083b
Ruri Meiko <chandoralong@gmail.com>
2024-11-07 00:13:48 +0700

    gộp seach ui x2

--- frontend/src/components/layout/ChatLayout/RightSidebar/RightSidebar.js
+++ frontend/src/components/layout/ChatLayout/RightSidebar/RightSidebar.js
@@ -11,12 +11,23 @@ import getChatRoom from "services/getchatroom";
 import FriendItem from "./FriendItem";
 import Search from "./Footer/Search";
 import getFriendList from "services/getFriendList";
+import findUser from "services/findUserService";
+
+
 const cx = classNames.bind(styles);
+
+
+
+
+
+
+
 const RightSidebar = () => {
     const { currUser } = useContext(ChatDataContext);
     const [chatRoom, setRoomData] = useState([]);
     const [pageState, setPageData] = useState('chat');
     const [friend, setFriend] = useState('chat');
+    const [findData, setFindData] = useState([]);
 
     const fetchChatRoom = useCallback(async () => {
         try {
@@ -26,6 +37,7 @@ const RightSidebar = () => {
             console.error('Error fetching new messages:', error);
         }
     }, [currUser]);
+
     const fetchFriendList = useCallback(async () => {
         try {
             const response = await getFriendList({ id: currUser.id });
@@ -35,12 +47,25 @@ const RightSidebar = () => {
         }
     }, [currUser]);
 
+    const handleSearchChange = async (e) => {
+        const data = {
+            text: e.target.value
+        }
+        if (data.text === '' || data.text === null || data.text === undefined || !data.text) {
+            setFindData([])
+            return
+        } else {
+            const res = await findUser(data);
+            setFindData(res.DT)
+        }
+    }
+
     useEffect(() => {
         fetchChatRoom();
         fetchFriendList();
     }, [fetchChatRoom, fetchFriendList]);
 
- 
+
 
     if (!currUser) return null;
     return (
@@ -81,7 +106,25 @@ const RightSidebar = () => {
                                 <div>寂し犬</div>
                             );
                         case 'search':
-                            return <Search></Search>;
+                            return <div>
+                                {findData && findData.map((item) => (
+                                    item.loai === 'Người dùng' ?
+                                        <ChatRoom
+                                            key={item.id}
+                                            id={item.id}
+                                            name={item.firstname + ' ' + item.lastname}
+                                            avt={item.avatar}
+                                        />
+                                        :
+                                        <ChatRoom
+                                            key={item.id}
+                                            id={item.id}
+                                            name={item.groupname}
+                                            avt={item.avatar}
+                                        />
+                                ))}
+                                <Search onChange={handleSearchChange}></Search>;
+                            </div>
                         case 'archive':
                             return <div style={{ color: 'white' }}>Archive Page</div>;
                         default:
@@ -94,113 +137,90 @@ const RightSidebar = () => {
     );
 };
 
-export default RightSidebar;import React, { useContext, useEffect, useState, useCallback } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import "./RightSidebar.scss";
-import Footer from "./Footer";
-import ChatRoom from "./chatRoom";
-import ChatDataContext from 'lib/Context/ChatContext';
-import getChatRoom from "services/getchatroom";
-import { timePassed } from "lib/function/formatTime";
-import findUser from "services/findUserService";
 
-const RightSidebar = () => {
-    const { currUser } = useContext(ChatDataContext);
-    const [chatRoom, setRoomData] = useState([]);
-    const [sidebar, setSidebar] = useState(null);
-    const [findData, setFindData] = useState([]);
+// const RightSidebar = () => {
+//     const { currUser } = useContext(ChatDataContext);
+//     const [chatRoom, setRoomData] = useState([]);
+//     const [sidebar, setSidebar] = useState(null);
+//     const [findData, setFindData] = useState([]);
 
-    const handleChange = async (e) => {
-        const data = {
-            text: e.target.value
-        }
-        if (data.text === '' || data.text === null || data.text === undefined || !data.text) {
-            setFindData([])
-            return
-        } else {
-            const res = await findUser(data)
-            setFindData(res.DT)
-        }
-    }
+//     const fetchChatRoom = useCallback(async () => {
+//         try {
+//             const response = await getChatRoom({ id: currUser.id });
+//             setRoomData(response.DT); // API trả về danh sách phòng chat và tin nhắn mới nhất
+//         } catch (error) {
+//             console.error('Error fetching new messages:', error);
+//         }
+//     }, [currUser]);
 
-    const fetchChatRoom = useCallback(async () => {
-        try {
-            const response = await getChatRoom({ id: currUser.id });
-            setRoomData(response.DT); // API trả về danh sách phòng chat và tin nhắn mới nhất
-        } catch (error) {
-            console.error('Error fetching new messages:', error);
-        }
-    }, [currUser]);
+// useEffect(() => {
+//     fetchChatRoom();
+// }, [fetchChatRoom, currUser]);
 
-    useEffect(() => {
-        fetchChatRoom();
-    }, [fetchChatRoom, currUser]);
+// const handleSidebar = (value) => {
+//     if (value === "search") {
+//         const newSidebarState = sidebar === "search" ? null : "search";
+//         setSidebar(newSidebarState);
+//         if (newSidebarState === "search") {
+//             setRoomData([]);
+//         } else {
+//             fetchChatRoom();
+//         }
+//     }
+// }
 
-    const handleSidebar = (value) => {
-        if (value === "search") {
-            const newSidebarState = sidebar === "search" ? null : "search";
-            setSidebar(newSidebarState);
-            if (newSidebarState === "search") {
-                setRoomData([]);
-            } else {
-                fetchChatRoom();
-            }
-        }
-    }
+// if (!currUser) return null;
 
-    if (!currUser) return null;
+// return (
+//     <div className="rightsidebar">
+//         <div className="me-auto list_nav">
+//             <div className="sidebar_header">
+//                 <FontAwesomeIcon icon={faUser} /> Wtf Chat
+//             </div>
+//             {sidebar === "search" ?
+//                 <input className="search" type="text" placeholder="Search" onChange={handleChange} />
+//                 :
+//                 ""
+//             }
+// {
+//     sidebar === "search" ?
+//         findData && findData.map((item) => (
+//             item.loai === 'Người dùng' ?
+//                 <ChatRoom
+//                     key={item.id}
+//                     id={item.id}
+//                     name={item.firstname + ' ' + item.lastname}
+//                     avt={item.avatar}
+//                 />
+//                 :
+//                 <ChatRoom
+//                     key={item.id}
+//                     id={item.id}
+//                     name={item.groupname}
+//                     avt={item.avatar}
+//                 />
+//         ))
+//         :
+//         <div>
+//             {chatRoom.length > 0 ? (
+//                 chatRoom.map((friend) => (
+//                     <ChatRoom
+//                         key={friend.id}
+//                         id={friend.id}
+//                         name={`${friend.first_name} ${friend.last_name}`}
+//                         avt={friend.avt}
+//                         time={timePassed(friend.last_message_time)}
+//                         mess={friend.last_message_content}
+//                     />
+//                 ))
+//             ) : (<div>No new messages</div>)}
+//         </div>
+// }
 
-    return (
-        <div className="rightsidebar">
-            <div className="me-auto list_nav">
-                <div className="sidebar_header">
-                    <FontAwesomeIcon icon={faUser} /> Wtf Chat
-                </div>
-                {sidebar === "search" ?
-                    <input className="search" type="text" placeholder="Search" onChange={handleChange} />
-                    :
-                    ""
-                }
-                {
-                    sidebar === "search" ?
-                        findData && findData.map((item) => (
-                            item.loai === 'Người dùng' ?
-                                <ChatRoom
-                                    key={item.id}
-                                    id={item.id}
-                                    name={item.firstname + ' ' + item.lastname}
-                                    avt={item.avatar}
-                                />
-                                :
-                                <ChatRoom
-                                    key={item.id}
-                                    id={item.id}
-                                    name={item.groupname}
-                                    avt={item.avatar}
-                                />
-                        ))
-                        :
-                        <div>
-                            {chatRoom.length > 0 ? (
-                                chatRoom.map((friend) => (
-                                    <ChatRoom
-                                        key={friend.id}
-                                        id={friend.id}
-                                        name={`${friend.first_name} ${friend.last_name}`}
-                                        avt={friend.avt}
-                                        time={timePassed(friend.last_message_time)}
-                                        mess={friend.last_message_content}
-                                    />
-                                ))
-                            ) : (<div>No new messages</div>)}
-                        </div>
-                }
-
-            </div>
-            <Footer sidebar={handleSidebar} />
-        </div>
-    );
-};
+//             </div >
+//     <Footer sidebar={handleSidebar} />
+//         </div >
+//     );
+// };
 
 export default RightSidebar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import GlobalStyles from './components/GlobalStyles';
 import ChatDataProvider from 'lib/provider/ChatDataProvider';
 
