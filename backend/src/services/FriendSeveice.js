55f44acafbb5e404da9531fe096d6a3dce06d318
Ruri Meiko <chandoralong@gmail.com>
2024-11-07 01:54:55 +0700

    friendlist

--- backend/src/services/FriendSeveice.js
+++ backend/src/services/FriendSeveice.js
@@ -4,10 +4,36 @@ import pool from '../connectDB.js';
 const getFriendList = async (userId, friendId) => {
     try {
         // Lấy ID phòng chat giữa hai người dùng
-        const [roomRows] = await pool.query(
-            `SELECT DISTINCT * FROM banbe WHERE useroneid = ? OR usertwoid = ?`,
-            [userId,userId]
+        const [friendRows] = await pool.query(
+            `SELECT nguoidung.id, nguoidung.avatar, nguoidung.firstname, nguoidung.lastname 
+            FROM nguoidung 
+            JOIN (
+                SELECT DISTINCT CASE 
+                    WHEN useroneid = ? THEN usertwoid 
+                    ELSE useroneid 
+                END as friendId 
+                FROM banbe 
+                WHERE useroneid = ? OR usertwoid = ?
+            ) AS friends ON nguoidung.id = friends.friendId`,
+            [userId, userId, userId]
         );
+
+        if (friendRows.length === 0) {
+            return {
+                EM: 'No friends found',
+                EC: 0,
+                DT: []
+            };
+        }
+
+        console.log('SERVICE | GET FRIEND SERVICE | FRIEND ROWS | ', friendRows);
+        return {
+            EM: 'Friends retrieved successfully',
+            EC: 1,
+            DT: friendRows
+        };
+
+        
         console.log('SERVICE | GET FRIEND SERVICE | ROOM ROWS | ', roomRows);
         if (roomRows.length === 0) {
             return {
